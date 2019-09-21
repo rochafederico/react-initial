2a3e52122d300af4383c9a3fe62cc82209ec310c
Federico Rocha <federiconrocha@gmail.com>
2019-09-21 11:09:38 -0300

    initial

---
+++ src/components/home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class HomeComponent extends Component {
+    render() {
+        return (
+            <h1>Inicio</h1>
+        )
+    }
+}
+
+export default HomeComponent
